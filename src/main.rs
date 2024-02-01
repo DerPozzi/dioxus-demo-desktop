c274313c5926b06c759c70d98138eee16f6c8a06
Gianluca Pozzo <gpozzo@web.de>
2024-02-01 10:53:37 +0100

    Initial Setup

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
+fn main() {
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        h1 {
+            "Hello World"
+        }
+    })
+}
